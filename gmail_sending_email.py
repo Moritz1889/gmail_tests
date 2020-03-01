856dc2de65182f5213937cbc927206481988766d
Moritz1889 <b.babirecki@gmail.com>
2020-03-01 20:43:06 +0100

    Delete pyautogui
    This plugin is not necessary for running those files

--- gmail_sending_email.py
+++ gmail_sending_email.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
-import pyautogui as pg
 
 def tap_string_on_keyboard(string):
     actions = ActionChains(driver)
